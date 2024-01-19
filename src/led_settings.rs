892bf5d068934917b8d495de60ab0dcb98c83b9a
Jomer.Dev <Jomer.Dev@posteo.de>
2024-01-19 18:42:42 +0100

    Working led implementation, blink led slowly white on hard fault

---
+++ src/led_settings.rs
@@ -0,0 +1,269 @@
+use embassy_rp::{
+    clocks,
+    dma::{AnyChannel, Channel},
+    into_ref,
+    pio::{
+        Common, Config, FifoJoin, Instance, PioPin, ShiftConfig, ShiftDirection,
+        StateMachine, Pio, InterruptHandler,
+    },
+    Peripheral, PeripheralRef, gpio::{Output, Level, AnyPin}, Peripherals, bind_interrupts, peripherals::PIO0,
+};
+use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, signal::Signal};
+use embassy_time::Timer;
+use fixed::types::U24F8;
+use fixed_macro::fixed;
+use smart_leds::{RGB8, RGB};
+
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+
+
+static LED_SETTINGS: Signal<ThreadModeRawMutex, LedSettings> = Signal::new();
+
+pub struct Ws2812<'d, P: Instance, const S: usize, const N: usize> {
+    dma: PeripheralRef<'d, AnyChannel>,
+    sm: StateMachine<'d, P, S>,
+}
+
+impl<'d, P: Instance, const S: usize, const N: usize> Ws2812<'d, P, S, N> {
+    pub fn new(
+        pio: &mut Common<'d, P>,
+        mut sm: StateMachine<'d, P, S>,
+        dma: impl Peripheral<P = impl Channel> + 'd,
+        pin: impl PioPin,
+    ) -> Self {
+        into_ref!(dma);
+
+        // Setup sm0
+
+        // prepare the PIO program
+        let side_set = pio::SideSet::new(false, 1, false);
+        let mut a: pio::Assembler<32> = pio::Assembler::new_with_side_set(side_set);
+
+        const T1: u8 = 2; // start bit
+        const T2: u8 = 5; // data bit
+        const T3: u8 = 3; // stop bit
+        const CYCLES_PER_BIT: u32 = (T1 + T2 + T3) as u32;
+
+        let mut wrap_target = a.label();
+        let mut wrap_source = a.label();
+        let mut do_zero = a.label();
+        a.set_with_side_set(pio::SetDestination::PINDIRS, 1, 0);
+        a.bind(&mut wrap_target);
+        // Do stop bit
+        a.out_with_delay_and_side_set(pio::OutDestination::X, 1, T3 - 1, 0);
+        // Do start bit
+        a.jmp_with_delay_and_side_set(pio::JmpCondition::XIsZero, &mut do_zero, T1 - 1, 1);
+        // Do data bit = 1
+        a.jmp_with_delay_and_side_set(pio::JmpCondition::Always, &mut wrap_target, T2 - 1, 1);
+        a.bind(&mut do_zero);
+        // Do data bit = 0
+        a.nop_with_delay_and_side_set(T2 - 1, 0);
+        a.bind(&mut wrap_source);
+
+        let prg = a.assemble_with_wrap(wrap_source, wrap_target);
+        let mut cfg = Config::default();
+
+        // Pin config
+        let out_pin = pio.make_pio_pin(pin);
+        cfg.set_out_pins(&[&out_pin]);
+        cfg.set_set_pins(&[&out_pin]);
+
+        cfg.use_program(&pio.load_program(&prg), &[&out_pin]);
+
+        // Clock config, measured in kHz to avoid overflows
+        // TODO CLOCK_FREQ should come from embassy_rp
+        let clock_freq = U24F8::from_num(clocks::clk_sys_freq() / 1000);
+        let ws2812_freq = fixed!(800: U24F8);
+        let bit_freq = ws2812_freq * CYCLES_PER_BIT;
+        cfg.clock_divider = clock_freq / bit_freq;
+
+        // FIFO config
+        cfg.fifo_join = FifoJoin::TxOnly;
+        cfg.shift_out = ShiftConfig {
+            auto_fill: true,
+            threshold: 24,
+            direction: ShiftDirection::Left,
+        };
+
+        sm.set_config(&cfg);
+        sm.set_enable(true);
+
+        Self {
+            dma: dma.map_into(),
+            sm,
+        }
+    }
+
+    pub async fn write(&mut self, colors: &[RGB8; N]) {
+        // Precompute the word bytes from the colors
+        let mut words = [0u32; N];
+        for i in 0..N {
+            let word = (u32::from(colors[i].g) << 24)
+                | (u32::from(colors[i].r) << 16)
+                | (u32::from(colors[i].b) << 8);
+            words[i] = word;
+        }
+
+        // DMA transfer
+        self.sm.tx().dma_push(self.dma.reborrow(), &words).await;
+    }
+}
+
+pub enum RGBStatus {
+    Blue,
+    Red,
+    Green,
+    Color(u8, u8, u8),
+    Off
+}
+
+impl RGBStatus {
+    pub fn to_rgb(&self) -> RGB<u8> {
+        match self {
+            RGBStatus::Off => (0,0,0).into(),
+            RGBStatus::Red => (255,0,0).into(),
+            RGBStatus::Green => (0,255,0).into(),
+            RGBStatus::Blue => (0,0,255).into(),
+            RGBStatus::Color(r, g, b) => (*r, *g, *b).into()
+        }
+    }
+}
+
+pub enum LedStatus {
+    Off,
+    Red,
+    Green,
+    Blue
+}
+
+impl LedStatus {
+    pub fn to_bits(&self) -> [bool; 3] {
+        match self {
+            LedStatus::Off => [false, false, false],
+            LedStatus::Red => [true, false, false],
+            LedStatus::Green => [false, true, false],
+            LedStatus::Blue => [false, false, true],
+        }
+    }
+}
+
+pub struct LedSettings {
+    pub blink_ms: Option<u64>,
+    pub led_status: Option<LedStatus>,
+    pub rgb_status: Option<RGBStatus>,
+    pub led_blinking: Option<bool>,
+    pub rgb_blinking: Option<bool>,
+}
+
+impl Default for LedSettings {
+    fn default() -> Self {
+        LedSettings {
+            led_blinking: None,
+            rgb_blinking: None,
+            blink_ms: None,
+            led_status: None,
+            rgb_status: None
+        }
+    }
+}
+
+
+pub fn set_led_settings(set: LedSettings) {
+    LED_SETTINGS.signal(set);
+}
+
+pub fn set_led(status: LedStatus, blink: bool) {
+    set_led_settings(LedSettings {
+        led_blinking: Some(blink),
+        led_status: Some(status),
+        ..Default::default()
+    });
+} 
+
+pub fn set_rgb(status: RGBStatus, blink: bool) {
+    set_led_settings(LedSettings {
+        rgb_blinking: Some(blink),
+        rgb_status: Some(status),
+        ..Default::default()
+    });
+} 
+
+struct Pins([Output<'static, AnyPin>; 3]);
+
+
+
+#[embassy_executor::task]
+pub async fn led_runner(
+    p: Peripherals
+) {
+
+    let _pow = Output::new(p.PIN_11, Level::High);
+
+    let Pio { mut common, sm0, .. } = Pio::new(p.PIO0, Irqs);
+
+    const NUM_LEDS: usize = 1;
+    let mut rgb_data = [RGB8::default(); NUM_LEDS];
+    let rgb_data_off = [RGB8::default(); NUM_LEDS];
+
+    let mut ws = Ws2812::new(&mut common, sm0, p.DMA_CH0, p.PIN_12);
+
+    let led_green = Output::new(AnyPin::from(p.PIN_16), Level::Low); // PIN_16: User Led Green
+    let led_red = Output::new(AnyPin::from(p.PIN_17), Level::Low); // PIN_17: User Led Red
+    let led_blue = Output::new(AnyPin::from(p.PIN_25), Level::Low); // PIN_25: User Led Blue
+
+    let mut pins = Pins([led_red, led_green, led_blue]);
+
+    let mut led_blinking = false;
+    let mut rgb_blinking: bool = false;
+    let mut blink_ms: u64 = 500;
+    let mut led_list: [bool; 3] = LedStatus::Off.to_bits();
+    
+
+    loop {
+        if let Some(settings) = LED_SETTINGS.try_take() {
+            if let Some(new_led_blinking) = settings.led_blinking {
+                led_blinking = new_led_blinking;
+            }
+            if let Some(new_rgb_blinking) = settings.rgb_blinking {
+                rgb_blinking = new_rgb_blinking;
+            }
+            if let Some(new_led_status) = settings.led_status {
+                led_list = new_led_status.to_bits();
+                if !led_blinking {
+                    pins.0.iter_mut()
+                        .zip(led_list.into_iter())
+                        .for_each(|(p, c)| if c { p.set_low() } else { p.set_high() });
+                }
+            }
+            if let Some(ms) = settings.blink_ms {
+                blink_ms = ms;
+            }
+            if let Some(status) = settings.rgb_status {
+                rgb_data[0] = status.to_rgb();
+            }
+
+            LED_SETTINGS.reset();
+        }
+
+        if led_blinking {
+            pins.0.iter_mut()
+                .zip(led_list.into_iter())
+                .for_each(|(p, c)| if c { p.set_low() } else { p.set_high() });
+        }
+        if rgb_blinking {
+            ws.write(&rgb_data).await;
+        }
+        Timer::after_millis(blink_ms).await;
+
+        if led_blinking {
+            pins.0.iter_mut()
+                .for_each(|p| p.set_high());
+        }
+        if rgb_blinking {
+            ws.write(&rgb_data_off).await;
+        }
+        Timer::after_millis(blink_ms).await;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,73 +2,70 @@
 #![no_std]
 #![no_main]
 
-use defmt::debug;
+use cortex_m_rt::{exception, ExceptionFrame};
 use embassy_executor::Spawner;
-use embassy_rp::{gpio::{Level, Output}, pio::{Pio, InterruptHandler}, bind_interrupts, peripherals::PIO0};
-use embassy_time::Timer;
-use smart_leds::RGB8;
+use embassy_rp::{Peripherals, gpio::{Level, Output}};
+use embassy_time::{Timer, Instant, Duration};
+use led_settings::{set_led, LedStatus};
+
+use crate::led_settings::led_runner;
 use {defmt_rtt as _, panic_probe as _};
 
-mod ws2812;
+mod led_settings;
 
-bind_interrupts!(struct Irqs {
-    PIO0_IRQ_0 => InterruptHandler<PIO0>;
-});
 
-/// Input a value 0 to 255 to get a color value
-/// The colours are a transition r - g - b - back to r.
-fn wheel(mut wheel_pos: u8) -> RGB8 {
-    wheel_pos = 255 - wheel_pos;
-    if wheel_pos < 85 {
-        return (255 - wheel_pos * 3, 0, wheel_pos * 3).into();
-    }
-    if wheel_pos < 170 {
-        wheel_pos -= 85;
-        return (0, wheel_pos * 3, 255 - wheel_pos * 3).into();
-    }
-    wheel_pos -= 170;
-    (wheel_pos * 3, 255 - wheel_pos * 3, 0).into()
-}
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
-    let Pio { mut common, sm0, .. } = Pio::new(p.PIO0, Irqs);
 
-    // PIN_16: User Led Green
-    // PIN_17: User Led Red
-    // PIN_25: User Led Blue
+    let _ = spawner.spawn(led_runner(p));
 
-    let _ = Output::new(p.PIN_16, Level::Low);
-    let _ = Output::new(p.PIN_17, Level::Low);
-    let _ = Output::new(p.PIN_25, Level::Low);
-    let _led_11 = Output::new(p.PIN_11, Level::High);
+    let fut = async {
+        set_led(LedStatus::Red, true);
 
-    const NUM_LEDS: usize = 1;
-    let mut data = [RGB8::default(); NUM_LEDS];
+        Timer::after_secs(5).await;
 
-    let mut ws = ws2812::Ws2812::new( &mut common, sm0, p.DMA_CH0, p.PIN_12);
+        set_led(LedStatus::Green, true);
 
+        Timer::after_secs(5).await;
+
+        set_led(LedStatus::Blue, true);
+
+        Timer::after_secs(5).await;
+
+        set_led(LedStatus::Off, false);
+    };
+
+    fut.await;
+}
+
+#[exception]
+unsafe fn HardFault(_frame: &ExceptionFrame) -> ! {
+    let p = unsafe { Peripherals::steal() };
+    let mut led1 = Output::new(p.PIN_16, Level::Low);
+    let mut led2 = Output::new(p.PIN_17, Level::Low);
+    let mut led3 = Output::new(p.PIN_25, Level::Low);
+    led1.set_low();
+    led2.set_low();
+    led3.set_low();
+    let mut instance = Instant::now();
+    let duration = Duration::from_millis(2000);
+    let mut low = true;
     loop {
-        // info!("led on!");
-        // led.set_high();
-        // Timer::after_secs(1).await;
-
-        // info!("led off!");
-        // led.set_low();
-        // Timer::after_secs(1).await;
-
-        for j in 0..(256 * 5) {
-            debug!("New Colors:");
-            for i in 0..NUM_LEDS {
-                data[i] = wheel((((i * 256) as u16 / NUM_LEDS as u16 + j as u16) & 255) as u8);
-                debug!("R: {} G: {} B: {}", data[i].r, data[i].g, data[i].b);
+        if Instant::now() == instance + duration {
+            instance = Instant::now();
+            low = !low;
+            if low {
+                led1.set_low();
+                led2.set_low();
+                led3.set_low();
+            } else {
+                led1.set_high();
+                led2.set_high();
+                led3.set_high();
             }
-            ws.write(&data).await;
-
-            Timer::after_millis(10).await;
         }
     }
 }
-
